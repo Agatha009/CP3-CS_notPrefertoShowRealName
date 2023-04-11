32aad68847abb8e5150fabeb20340e6db4f0e495
Agatha009 <85536187+Agatha009@users.noreply.github.com>
2023-04-11 16:52:56 +0700

    Add files via upload

---
+++ Exercise4_Chatchai_S.py
@@ -0,0 +1,10 @@
+print("-"*15, "Your Score", "-"*15)
+SFE = 88
+SGB = 92
+SIC = 79.8
+SCP = 94
+print("Foundation English:","             ", "-"*3,SFE)
+print("General Business:","               ", "-"*3,SGB)
+print("Introduction to Computer Systems:"  , "-"*3,SIC)
+print("Computer Programming:","           ", "-"*3,SCP)
+print("-"*13, "Congratulations", "-"*12)
